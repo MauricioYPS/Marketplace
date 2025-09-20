110d11c21ce2fb375dff1631bef3bab83c1ba4ab
Juan Carlos Forero <juan.forero66@gmail.com>
2025-09-19 19:09:35 -0500

    prueba1

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-// --- Íconos para la UI ---
 const LogoIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
